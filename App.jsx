3327283e476423a3d24e93117dd6dbf6c1b5c224
Akhil Choubey <62749779+AkhilChoubey@users.noreply.github.com>
2021-10-04 01:55:15 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Register from "./Register";
+
+function App() {
+  return (
+    <div className="container">{
+        <Register />
+    }  
+    </div>
+  );
+}
+
+export default App;
+ 
